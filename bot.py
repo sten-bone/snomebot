a44c3e4d176c501d98acf51ee5640c87910659ff
sten-bone <49705989+sten-bone@users.noreply.github.com>
2021-10-19 14:25:51 -0500

    Add basic message events

--- bot.py
+++ bot.py
@@ -4,11 +4,38 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+# allows bot to access members
+botIntents = discord.Intents.default()
+botIntents.members = True
+
+client = discord.Client(intents=botIntents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    
+    print(f'{client.user} has connected to Guild: {guild.name}!')
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    print(message)
+    await message.channel.send("Thanks for the message!")
+
+# handle errors from on_message()
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('err.log', 'a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
+
 
 client.run(TOKEN)
