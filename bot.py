fd82085ebf85e92cfbb4ea12adbb3b197834b96d
sten-bone <49705989+sten-bone@users.noreply.github.com>
2021-10-19 14:32:12 -0500

    Change Client to Bot

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,26 +11,27 @@ GUILD = os.getenv('DISCORD_GUILD')
 botIntents = discord.Intents.default()
 botIntents.members = True
 
-client = discord.Client(intents=botIntents)
+# client = discord.Client(intents=botIntents)
+bot = commands.Bot(command_prefix="sb ", intents=botIntents)
 
-@client.event
+@bot.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    guild = discord.utils.get(bot.guilds, name=GUILD)
     
-    print(f'{client.user} has connected to Guild: {guild.name}!')
+    print(f'{bot.user} has connected to Guild: {guild.name}!')
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     
     print(message)
     await message.channel.send("Thanks for the message!")
 
 # handle errors from on_message()
-@client.event
+@bot.event
 async def on_error(event, *args, **kwargs):
     with open('err.log', 'a') as f:
         if event == 'on_message':
@@ -38,4 +40,4 @@ async def on_error(event, *args, **kwargs):
             raise
 
 
-client.run(TOKEN)
+bot.run(TOKEN)
