1da6df9aacdee763cfbd78f74a6e6e50cf9990a6
sten-bone <49705989+sten-bone@users.noreply.github.com>
2021-10-19 22:20:05 -0500

    Add roll command

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from random import randint
+
+DICE = [':one:', ':two:', ':three:', ':four:', ':five:', ':six:']
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -17,7 +20,6 @@ bot = commands.Bot(command_prefix="sb ", intents=botIntents)
 @bot.event
 async def on_ready():
     guild = discord.utils.get(bot.guilds, name=GUILD)
-    print(guild.members)
     
     print(f'{bot.user} has connected to Guild: {guild.name}!')
     members = '\n - '.join([f'{member.name} ({member.top_role})' for member in guild.members])
@@ -28,6 +30,20 @@ async def on_ready():
 async def hello(context):
     # context is a Context object
     await context.send(f"Howdy {context.message.author.mention}!")
+
+@bot.command(name='roll', help='Roll 2d6.')
+async def roll(context):
+    author = context.message.author.mention
+    one, two = roll_dice()
+    await context.send(f"{author}\n**Result :game_die:**\n{show_die(one)} {show_die(two)}")
+
+# roll 2d6 and return results
+def roll_dice():
+    return randint(1,6), randint(1, 6)
+
+# return string representation of dice
+def show_die(pips : int):
+    return DICE[pips - 1]
 
 # handle errors from on_message()
 @bot.event
