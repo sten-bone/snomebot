6113f38a1e0e76658f8c07eff038e9a676e62852
sten-bone <49705989+sten-bone@users.noreply.github.com>
2021-10-19 22:06:23 -0500

    Add command roles

--- bot.py
+++ bot.py
@@ -17,15 +17,17 @@ bot = commands.Bot(command_prefix="sb ", intents=botIntents)
 @bot.event
 async def on_ready():
     guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(guild.members)
     
     print(f'{bot.user} has connected to Guild: {guild.name}!')
-    members = '\n - '.join([member.name for member in guild.members])
+    members = '\n - '.join([f'{member.name} ({member.top_role})' for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
-@bot.command(name='hello')
+@bot.command(name='hello', help='Responds to a message.')
+@commands.has_role('admin')
 async def hello(context):
     # context is a Context object
-    await context.send("Howdy!")
+    await context.send(f"Howdy {context.message.author.mention}!")
 
 # handle errors from on_message()
 @bot.event
@@ -36,5 +38,10 @@ async def on_error(event, *args, **kwargs):
         else:
             raise
 
+# handle command errors
+@bot.event
+async def on_command_error(context, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await context.send(f"Sorry {context.message.author.mention}, you do not have permission to use this command.")
 
 bot.run(TOKEN)
